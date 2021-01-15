1e2d30d454b7c137087e3092651600a634a6ac41
Adrian Cretan <vasile.cretan@stud.acs.upb.ro>
2021-01-15 14:44:07 +0200

    Testare Suma2

---
+++ suma.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	int a,b,suma;
+
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	suma = a+b;
+
+	printf(Suma dintre %d si %d este: %d\n, a,b,suma);
+
+	return 0;
